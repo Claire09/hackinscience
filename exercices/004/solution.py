aa76b423943cc687f7f45b91812fc8a8218e42ed
Claire09 <clairemayer09@gmail.com>
2015-09-22 14:54:43 +0200

    Update solution.py

--- exercices/004/solution.py
+++ exercices/004/solution.py
@@ -1,9 +1,9 @@
 import string
 letters=string.ascii_lowercase[::-1]
-letters = letters.replace("a", "A")
-letters = letters.replace("e", "E")
-letters = letters.replace("i", "I")
-letters = letters.replace("o", "O")
-letters = letters.replace("u", "U")
-letters = letters.replace("y", "Y")
+letters=letters.replace("a", "A")
+letters=letters.replace("e", "E")
+letters=letters.replace("i", "I")
+letters=letters.replace("o", "O")
+letters=letters.replace("u", "U")
+letters=letters.replace("y", "Y")
 print(letters)
